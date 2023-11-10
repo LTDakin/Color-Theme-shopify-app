9aa64419100924bd09d1868f2c694c153b1e2cb8
LTDakin <taidakin99lol@gmail.com>
2023-11-09 18:02:46 -0800

    Creating a new util file This is the file that will contain color algorithim logic for generating dominant colors

---
+++ Color-Theme-App/web/frontend/utils/dominant_color_util.js
@@ -0,0 +1,3 @@
+// This file will be where the logic and functions that return a dominant color from a list of images [0..n] reside
+
+// function generate_dominant_colors(list of images[], top_n_dominant_colors)
