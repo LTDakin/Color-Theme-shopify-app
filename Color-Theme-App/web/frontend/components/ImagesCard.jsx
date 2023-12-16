4af464cd025614002ba4afd02932d365438e58b3
LTDakin <taidakin99lol@gmail.com>
2023-12-15 18:19:07 -0800

    added button

--- Color-Theme-App/web/frontend/components/ImagesCard.jsx
+++ Color-Theme-App/web/frontend/components/ImagesCard.jsx
@@ -1,10 +1,13 @@
+import { Card, Text, Button } from "@shopify/polaris";
 import React from "react";
-import { Card, Text } from "@shopify/polaris";
 
 export function ImagesCard() {
   return (
     <>
-      <Card>Items that are being used to generate the color palletes</Card>
+      <Card padding="500">
+        Items that are being used to generate the color palletes
+        <Button>Fetch Products</Button>
+      </Card>
     </>
   );
 }
